fab44818b820d769e99949f5da22eca485f635ed
abc <qq1651702207@163.com>
2023-04-01 20:23:20 +0800

    测试优化ui

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,69 +1,86 @@
 <script setup lang="ts">
-    import Nav from "./FNav.vue";
+    import { ref,onMounted } from "vue";
     import {RouterView} from 'vue-router'
-    import { ref,onBeforeUnmount } from "vue";
+    import FLNav from "./FLNav.vue";
+    import FRNav from "./FRNav.vue";
     import Tail from "../Tail.vue";
-    import FHead from "./FHead.vue";
-
-    // 动态改变高度
-    const rootHeight = ref(window.innerHeight)
-    let i = true;
-    function changeRootHeight(){
-        rootHeight.value = window.innerHeight;
-        i=true
-    }
-    function resize() {
-        if(i){
-            i=false
-            window.setTimeout(changeRootHeight,100)
-        }
-    }
-    window.addEventListener('resize',resize)
-    onBeforeUnmount(()=>{
-        window.removeEventListener('resize',resize)
+    import FHead from "./FTHead.vue";
+    import BackCan from '../wheel/BackCan.vue'
+    let main = ref();
+    let height = ref(0)
+    onMounted(()=>{
+        height.value = main.value.offsetHeight
     })
+
 </script>
 <template>
     <div class="root">
-        <!-- 左侧导航 -->
-        <Nav class="navr" :style="{height:rootHeight+'px'}"></Nav>
-        <!-- 主体 -->
-        <div class="main" :style="{height:rootHeight+'px'}">
-            <!-- 主体固定头部 -->
-            <FHead class="main-head" />
-            <!-- 主体内容 -->
-            <div>
-                <RouterView v-slot="{Component}">
-                    <KeepAlive>
-                        <component :is='Component' />
-                    </KeepAlive>
-                </RouterView>
-                <Tail />
-            </div>
+        <!-- 左侧固定导航 -->
+        <FLNav class="stick" />
+        <div class="bo">
+            <!-- 固定头部 -->
+            <FHead class="stick" />
+            <main ref="main" class="main">
+                <!-- 主体内容 -->
+                <div>
+                    <RouterView v-slot="{Component}">
+                        <KeepAlive>
+                            <component :is='Component' />
+                        </KeepAlive>
+                    </RouterView>
+                    <Tail />
+                </div>
+                <BackCan class="back-can" :heigh="height" />
+            </main>
         </div>
+        <!-- 右侧导航 -->
+        <FRNav class="show" />
     </div>
 </template>
 <style scoped>
-    .root{
-        display: flex;
+.root{
+    display: flex;
+    justify-content: space-between;
+    /* *** */
+}
+.bo{
+    flex-grow: 1;
+    border-left: 1px solid rgb(223, 223, 223);
+    border-right: 1px solid rgb(223, 223, 223);
+    /* overflow: clip; */
+}
+.main{
+    position: relative;
+    overflow-x: hidden;
+    top: 0;
+}
+.main > div{
+    width: 700px;
+    margin: 0px 30px 60px;
+}
+.back-can{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    z-index: -1;
+}
+.stick{
+    position: sticky;
+    top: 0px;
+}
+.show{
+    box-sizing: border-box;
+    width: 300px;
+    margin: 0 30px;
+}
+@media (max-width:1250px){
+    .show{
+        width: 240px;
     }
-    .navr{
-        flex-shrink: 0;
-        overflow: hidden;
-    }
-    .main{
-        flex-grow: 1;
-        max-width:800px;
-        position: relative;
-        overflow-y: auto;
-        overflow-x: hidden;
-    }
-    .main::-webkit-scrollbar{
+}
+@media (max-width:1150px) {
+    .show{
         display: none;
     }
-    .main-head{
-        position: sticky;
-        top: 0px;
-        z-index: 1;
-    }
+}
 </style>
