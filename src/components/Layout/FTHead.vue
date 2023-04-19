06a759a1c904f69316e6f16eb774776d8d1a9351
abc <qq1651702207@163.com>
2023-04-19 18:03:19 +0800

    optimize:changeCss

--- src/components/Layout/FTHead.vue
+++ src/components/Layout/FTHead.vue
@@ -14,7 +14,7 @@ import FTsearch from "./FTsearch.vue";
     padding-bottom: 20px;
     justify-content: space-evenly;
     backdrop-filter: blur(4px);
-    background-image: radial-gradient(white 0%,rgba(255, 255, 255, 0.5) 25%,rgba(255, 255, 255, 0.7) 30%,white 40%);
+    background-image: radial-gradient(white 0%,rgba(255, 255, 255, 0.5) 25%,rgba(255, 255, 255, 0.5) 30%,white 40%);
     background-size: 5px 5px;
     border-bottom: 1px solid white;
     z-index: 1;
