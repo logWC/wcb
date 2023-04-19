9b788bbd9110dd430a1aa029495ea6123f7a9dab
abc <qq1651702207@163.com>
2023-04-19 18:08:25 +0800

    add_optimize:LHpersonalFm_LHpersonalizedMv_add,addSlot

--- src/components/Layout/LHHome.vue
+++ src/components/Layout/LHHome.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import LHsignin from "./LHsignin.vue"
+// import LHsignin from "./LHsignin.vue"
+import LeftTiele from "../wheel/LeftTiele.vue";
+import LHpersonalFm from "./LHpersonalFm.vue";
+import LHpersonalizedMv from "./LHpersonalizedMv.vue";
 import LHrecommendSongSheet from "./LHrecommendSongSheet.vue";
 import LHtopic from "./LHtopic.vue";
 import $api from '@/api/index'
@@ -9,14 +12,17 @@ import { ref } from "vue";
     const recommendList = ref(null)
     const new_data = ref(null)
     const hot_data = ref(null)
-    Promise.all([$api.recommend(),$api.playlistTrackAll(3779629),$api.playlistTrackAll(3778678)])
-    .then(([song_datas,new_datas,hot_datas])=>{
+    const mvList = ref(null)
+    Promise.all([$api.recommend(),$api.playlistTrackAll(3779629),$api.playlistTrackAll(3778678),$api.personalizedMv()])
+    .then(([song_datas,new_datas,hot_datas,personalizedMv_datas])=>{
         if(!song_datas.data.recommend)throw new Error("未登录");
         // 推荐歌单
         recommendList.value = song_datas.data.recommend
         // 排行榜
         new_data.value = new_datas.data
         hot_data.value = hot_datas.data
+        // 推荐mv
+        mvList.value = personalizedMv_datas.data
         // 子组件骨架状态
         loading.value = false
     })
@@ -24,8 +30,19 @@ import { ref } from "vue";
 </script>
 <template>
     <div>
-        <LHsignin />
-        <LHrecommendSongSheet :songList="recommendList" :loading="loading" />
-        <LHtopic :loading="loading" :new_data="new_data" :hot_data="hot_data" />
+        <!-- <LHsignin /> -->
+        <LHrecommendSongSheet :songList="recommendList" :loading="loading" >
+            <LeftTiele str="定制推荐" />
+        </LHrecommendSongSheet>
+        <LHpersonalFm />
+        <LHtopic :loading="loading" :new_data="new_data" :hot_data="hot_data" >
+            <template #newsong>
+                <LeftTiele str="新歌top10"/>
+            </template>
+            <template #hotsong>
+                <LeftTiele str="热歌top10" />
+            </template>
+        </LHtopic>
+        <LHpersonalizedMv :loading="loading" :mvList="mvList" />
     </div>
 </template>
